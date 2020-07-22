f508ca74000a14fff6f24ac3229259037dd92c62
Sayana <nisayana23@yandex.ru>
2020-07-22 23:23:46 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,9 +9,8 @@ def join_nested_strings(src)
     element_index = 0
     while element_index < src[row_index].count do
       # binding.pry
-      if src[row_indexe][element_index].class == String
+      if src[row_index][element_index].class == String
       combined += src[row_index][element_index]
-
       end
       element_index +=1
     end
